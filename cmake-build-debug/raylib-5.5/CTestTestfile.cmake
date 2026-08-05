f72fc4665a1144edee1f0ad3e7d69bac4b081a39
unknown <wjayson@outlook.com>
2026-08-05 22:53:18 +0800

    Apply gitignore rules

--- cmake-build-debug/raylib-5.5/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/CLionProjects/starship/raylib-5.5
-# Build directory: E:/CLionProjects/starship/cmake-build-debug/raylib-5.5
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
--- cmake-build-debug/raylib-5.5/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/CLionProjects/starship/raylib-5.5/src
-# Build directory: E:/CLionProjects/starship/cmake-build-debug/raylib-5.5/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
